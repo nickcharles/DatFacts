606e16aeaf1df5ccb745b1efc3453d60c0cfc533
Nathan Riley <rileynat@umich.edu>
2015-01-17 02:16:17 -0500

    fucking changes mate

--- controllers/main.py
+++ controllers/main.py
@@ -1,7 +1,45 @@
 from flask import *
+from firebase import firebase
 
 main = Blueprint('main', __name__, template_folder='views')
 
 @main.route('/')
 def main_route():
-    return render_template("index.html")
+	if request.method == 'POST':
+		phoneNum = checkPhoneNum(request.form['phoneNum'])
+		subject = checkSubject(subjecrequest.form['subject'])
+		addSubscriber(phoneNum, subject)
+
+	return render_template("index.html")
+
+@main.route('/unsubscribe')
+def unsubscribe():
+	if request.method == 'POST':
+		phoneNum = request.form['yourNum']
+		newNum = request.form['newNum']
+		subject = request.form['subject']
+		removeSubcriber(phoneNum)
+		addSubscriber(newNum, subject)
+	return render_template("unsub.html")
+
+
+def addSubscriber(number, subject):
+	return True
+
+def removeSubscriber(number):
+	return True
+
+def checkPhoneNum(number):
+	return number
+
+def checkSubject(subject):
+	if subject is None or subject == string.empty:
+		return 'cat'
+
+	return subject
+
+# @main.route('/replies')
+# def replies():
+# 	firebase = firebase.FirebaseApplication('https://luminous-fire-1975.firebaseio.com')
+# 	result = firebase.get('replies', None)
+# 	return render_template("replies.html")
