ed68b5ef34a7359be494ee33fa98f758456fa2ab
nickcharles <nickcmorgan@gmail.com>
2015-01-17 03:54:49 -0500

    This may be scary

--- controllers/main.py
+++ controllers/main.py
@@ -7,45 +7,56 @@ main = Blueprint('main', __name__, template_folder='views')
 
 @main.route('/', methods=['GET', 'POST'])
 def main_route():
-	if request.method == 'POST':
-		phoneNum = checkPhoneNum(request.form['phoneNum'])
-		subject = checkSubject(request.form['subject'])
-		addSubscriber(phoneNum, subject)
-		return render_template("index.html", phoneNum=phoneNum, subject=subject)
-	return render_template("index.html");
+    if request.method == 'POST':
+        number = request.form['number']
+        subject = request.form['subject']
+        handle(number, subject)
+        return redirect('/')
+    return render_template("index.html")
 
 @main.route('/unsubscribe', methods=['GET', 'POST'])
 def unsubscribe():
-	if request.method == 'POST':
-		phoneNum = request.form['yourNum']
-		newNum = request.form['newNum']
-		subject = request.form['subject']
-		removeSubcriber(phoneNum)
-		addSubscriber(newNum, subject)
-	return render_template("unsub.html")
+    if request.method == 'POST':
+        phoneNum = request.form['yourNum']
+        newNum = request.form['newNum']
+        subject = request.form['subject']
+        removeSubcriber(phoneNum)
+        addSubscriber(newNum, subject)
+    return render_template("unsub.html")
 
+
+def handle(number, subject):
+    # Validate input
+    (number, subject) = validateInput(number, subject)
+    # Add to database
+    # Begin texting
+    return render_template("index.html")
+
+def validateInput(number, subject):
+    number = validateNumber(number)
+    subject = validateSubject(subject)
+    return (number, subject)
+
+def validateNumber(number):
+    isPhoneNum = re.compile("^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$")
+    if not isPhoneNum.match(number):
+        return render_template("index.html")
+    number = re.sub("[^0-9]", "", number)
+    return number
+
+def validateSubject(subject):
+    if subject is None or subject == "":
+        return 'cat'
+    return subject
 
 def addSubscriber(number, subject):
-	return True
+    return True
 
 def removeSubscriber(number):
-	return True
-
-def checkPhoneNum(number):
-	isPhoneNum = re.compile("^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$");
-	if not isPhoneNum.match(number):
-		return None
-	number = re.sub("[^0-9]", "", number)
-	return number
-
-def checkSubject(subject):
-	# if subject is None or subject == string.empty:
-		# return 'cat'
-
-	return subject
+    return True
 
 # @main.route('/replies')
 # def replies():
-# 	firebase = firebase.FirebaseApplication('https://luminous-fire-1975.firebaseio.com')
-# 	result = firebase.get('replies', None)
-# 	return render_template("replies.html")
+#   firebase = firebase.FirebaseApplication('https://luminous-fire-1975.firebaseio.com')
+#   result = firebase.get('replies', None)
+#   return render_template("replies.html")
